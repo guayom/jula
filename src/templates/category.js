42658aef01b80cf50296f22e2d976031890e6622
guayom <guayo.mena@gmail.com>
2018-03-04 22:30:18 -0600

    Add images to category page

--- src/templates/category.js
+++ src/templates/category.js
@@ -7,6 +7,18 @@ const propTypes = {
   data: PropTypes.object.isRequired,
 }
 
+function Images({images}) {
+  if (!images) {
+      return <p>No Image</p>
+  }
+
+  return (
+    <div style={{height: `auto`, overflow: `hidden`, flex: `1 0`}}>
+      <Img sizes={images[0].sizes} />
+    </div>
+  )
+}
+
 class categoryTemplate extends React.Component {
   render() {
     const page = this.props.data.contentfulProjectCategory
@@ -21,10 +33,17 @@ class categoryTemplate extends React.Component {
             }}
           />
         <h2>Projects</h2>
-        <ul>
+        <ul style={{listStyle: `none`, margin: 0, padding: 0}}>
           {projects.map(project => (
-            <li key={project.node.id}>
-              <Link to={`/work/${project.node.slug}`}>{project.node.title}</Link>
+            <li 
+              key={project.node.id}
+              style={{
+                display: `flex`,
+                margin: `0 0 20px`
+              }}
+            >
+              <Images images={project.node.images} />
+              <Link style={{flex: `5 0`}} to={`/work/${project.node.slug}`}>{project.node.title}</Link>
             </li>
           ))}
         </ul>
@@ -56,6 +75,15 @@ export const categoryQuery = graphql`
           id
           title
           slug
+          images{
+            sizes{
+              srcSet
+              base64
+              aspectRatio
+              src
+              sizes
+            }
+          }
           projectCategory {
             id
           }
