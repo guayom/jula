f907635cc196c2a697b2ec9da1df5e19ea0676fa
Guayo Mena <guayo.mena@gmail.com>
2019-10-04 15:37:16 -0600

    Add Montserrat font

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,12 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-theme-ui`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: "Ju-la.com",
---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from "typography";
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.666,
+  googleFonts: {name: 'Montserrat', styles: [ '300', '500', '600' ] },
+  headerFontFamily: ["Montserrat"],
+  bodyFontFamily: ["Montserrat"]
+});
+export default typography;
