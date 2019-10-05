de3ba092fe8bee28a4b002ebfb0bf900d7a0798d
Guayo Mena <guayo.mena@gmail.com>
2019-10-05 14:12:23 -0600

    Add logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Wrapper from "../Layout/Wrapper";
 import { Box, Flex } from "rebass";
-import HeaderLink from "./HeaderLink";
 import Nav from "./Nav";
+import ReactSVG from "react-svg";
+import Link from "gatsby-link";
 
 const Header = () => (
   <Box bg="daintree" py={3}>
     <Wrapper>
       <Flex alignItems="center">
-        <Box ml={-2}>
+        <Box ml={-2} width={["100px","100px","150px"]}>
           <h1 style={{ margin: 0 }}>
-            <HeaderLink to="/">Ju-La.com</HeaderLink>
+            <Link to="/">
+              <ReactSVG
+                src="jula-logo.svg"
+                alt="Ju-la.com"
+                beforeInjection={svg => {
+                  svg.classList.add("svg-class-name");
+                  svg.setAttribute("style", "fill: white");
+                }}
+              />
+            </Link>
           </h1>
         </Box>
         <Box
