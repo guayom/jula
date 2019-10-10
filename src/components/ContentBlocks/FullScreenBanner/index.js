0812e9fdf43a4d35c06d9a6abcfb879d7dc3b679
Guayo Mena <guayo.mena@gmail.com>
2019-10-10 17:34:39 -0600

    Make homebanner image fullscreen on mobile

--- src/components/ContentBlocks/FullScreenBanner/index.js
+++ src/components/ContentBlocks/FullScreenBanner/index.js
@@ -14,7 +14,18 @@ export default ({ title, image, description }) => (
       height="100%"
       sx={{ position: `absolute`, top: 0, left: 0, zIndex: -1 }}
     >
-      <Img fluid={image.fluid} alt={title} />
+      <Img
+        fluid={image.fluid}
+        alt={title}
+        style={{
+          position: "absolute",
+          left: 0,
+          top: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1
+        }}
+      />
     </Box>
     <Wrapper>
       <Text as="h2" color="white" mb={3}>
