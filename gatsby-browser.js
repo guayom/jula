7c0f2219521c271c92bf7f96e25288a6aafe06dc
Guayo Mena <guayo.mena@gmail.com>
2019-10-10 15:38:59 -0600

    Add polyfill for intersection observer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,11 @@
 
  // You can delete this file if you're not using it
  require("typeface-montserrat"); 
+ 
+ export const onClientEntry = () => {
+   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+   if (typeof window.IntersectionObserver === `undefined`) {
+     import(`intersection-observer`);
+     console.log(`# IntersectionObserver is polyfilled!`);
+   }
+ };
