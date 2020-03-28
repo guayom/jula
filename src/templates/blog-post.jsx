6f9f172689b39e6f98668d36fcd8e28aef9f3096
guayom <guayo.mena@gmail.com>
2020-03-28 11:15:12 -0600

    Add content to blog posts

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import PageTitle from '../components/page-title'
 import Wrapper from "../components/Layout/Wrapper";
 import Layout from "../components/Layout";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-export default ({ data: {post: { title }} }) => {
+const Bold = ({ children }) => <span className="bold">{children}</span>
+const Text = ({ children }) => <p className="align-center">{children}</p>
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: text => <Bold>{text}</Bold>,
+  },
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+  },
+}
+
+export default ({ data: {post: { title, body }} }) => {
     return(
         <Layout>
             <Wrapper>
                 <PageTitle>{ title }</PageTitle>
+                {documentToReactComponents(body.json, options)}
             </Wrapper>
         </Layout>
     );
@@ -17,6 +32,9 @@ export const blogPostQuery = graphql`
   query blogPostQuery($slug: String) {
     post: contentfulPost(slug: { eq: $slug }) {
       title
+        body {
+            json
+        }
     }
   }
 `;
